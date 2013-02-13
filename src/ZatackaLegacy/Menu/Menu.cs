144815cc9a92e67fd1e42c584d8df5084742fcbc
Mate Solymosi <mate@solymo.si>
2013-02-13 16:16:16 +0100

    Added menu text for debugging.

--- src/ZatackaLegacy/Menu/Menu.cs
+++ src/ZatackaLegacy/Menu/Menu.cs
@@ -14,10 +14,8 @@ namespace Zatacka.Menu
             : base(Dispatcher)
         {
             Canvas.Background = new SolidColorBrush(Colors.DarkRed);
-            /*using (DrawingContext DC = Canvas.RenderOpen())
-            {
-                DC.DrawText(new FormattedText("This is the MENU.\r\nPress ENTER to switch to GAME state.\r\nPress ESC to quit.", System.Globalization.CultureInfo.CurrentCulture, FlowDirection.LeftToRight, new Typeface("Arial Black"), 40, Brushes.White), new Point(100, 100));
-            }*/
+
+            Canvas.Add(new Unit.Text(Canvas, "This is the MENU.\r\nPress ENTER to switch to GAME state.\r\nPress ESC to quit.", 48, new Rect(100, 100, 0, 0)));
         }
 
         public override void Input(Key Button)
